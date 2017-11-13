2d07e4e169215c47d8922771cf71d88f8cd256a2
NexFlare <harshmagarwal@outlook.com>
2017-11-14 02:20:16 +0530

    Contact permission added

--- app/src/main/java/com/nexflare/expensemanager/MainActivity.kt
+++ app/src/main/java/com/nexflare/expensemanager/MainActivity.kt
@@ -1,12 +1,48 @@
 package com.nexflare.expensemanager
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fab.setOnClickListener {
+            checkForPermission()
+        }
+
+    }
+
+    private fun checkForPermission() {
+        if(ActivityCompat.checkSelfPermission(this@MainActivity,Manifest.permission.READ_CONTACTS)!=PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.READ_CONTACTS),1234)
+        }
+        else{
+            showToast("Permission Given Earlier")
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode==1234){
+            if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                showToast("Permission Given")
+            }
+        }
+    }
+    /*inline fun View.snack(message: String, length: Int = Snackbar.LENGTH_SHORT, f: Snackbar.() -> Unit) {
+        val snack = Snackbar.make(this, message, length)
+        snack.f()
+        snack.show()
+    }*/
+    inline fun Context.showToast(message:String){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
     }
 }
