713702db25b78b776e85929479906e1eb6b0f9a3
NexFlare <harshmagarwal@outlook.com>
2017-11-16 23:06:28 +0530

    Radio button added dialog box

--- app/src/main/java/com/nexflare/expensemanager/Activity/ExpenseActivity.kt
+++ app/src/main/java/com/nexflare/expensemanager/Activity/ExpenseActivity.kt
@@ -6,9 +6,7 @@ import android.os.Bundle
 import android.support.design.widget.TextInputLayout
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
 import com.google.firebase.database.*
 import com.nexflare.expensemanager.*
 import com.nexflare.expensemanager.Adapter.ExpenseAdapter
@@ -21,12 +19,14 @@ class ExpenseActivity : AppCompatActivity() {
     private lateinit var databaseReference:DatabaseReference
     private lateinit var expenseAdapter: ExpenseAdapter
     lateinit var phone:String
+    lateinit var name:String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_expense)
         firebaseDatabase= FirebaseDatabase.getInstance()
         databaseReference=firebaseDatabase.getReference("expense")
         phone=intent.getStringExtra("phone")
+        name=intent.getStringExtra("name")
         expenseAdapter = ExpenseAdapter(this, ArrayList())
         expenseRv.layoutManager=LinearLayoutManager(this)
         expenseRv.adapter=expenseAdapter
@@ -38,9 +38,15 @@ class ExpenseActivity : AppCompatActivity() {
             val amountEt=dialog.findViewById<EditText>(R.id.amountEt)
             val reason=dialog.findViewById<EditText>(R.id.reasonEt)
             val okTv=dialog.findViewById<TextView>(R.id.okTv)
+            val radioGroup=dialog.findViewById<RadioGroup>(R.id.radioGroup)
+            val radioUser=dialog.findViewById<RadioButton>(R.id.user)
             val dateTil=dialog.findViewById<TextInputLayout>(R.id.dateTil)
             val dateEt=dialog.findViewById<EditText>(R.id.dateEt)
             val calenderIv=dialog.findViewById<ImageView>(R.id.calenderIv)
+            if(name.contains(" "))
+                radioUser.setText(name.toCharArray(),0,name.indexOf(" "))
+            else
+                radioUser.setText(name.toCharArray(),0,name.length)
             calenderIv.setOnClickListener{
                 Log.d("TAGGER","Clicked")
                 val dateFragment= DatePickerFragment.newInstance(object : DatePickerInterface {
@@ -53,7 +59,11 @@ class ExpenseActivity : AppCompatActivity() {
             }
             okTv.setOnClickListener{
                 val key=databaseReference.child(phone).push().key
-                databaseReference.child(phone).child(key).setValue(Expense(amountEt.text.toString().toLong(),
+                var amount=amountEt.text.toString().toLong()
+                if(radioGroup.checkedRadioButtonId == R.id.user){
+                    amount=amountEt.text.toString().toLong()*-1
+                }
+                databaseReference.child(phone).child(key).setValue(Expense(amount,
                         reason.text.toString(),
                         dateEt.text.toString()))
                 firebaseDatabase.getReference("users").orderByChild("phone").equalTo(phone).addListenerForSingleValueEvent(object :ValueEventListener{
@@ -65,7 +75,7 @@ class ExpenseActivity : AppCompatActivity() {
                         Log.d("TAGGER",p0?.hasChildren().toString())
                         for(userSnapshot:DataSnapshot in p0!!.children)
                             //Log.d("TAGGER",userSnapshot.child("total").value.toString())
-                            userSnapshot.ref?.child("total")?.setValue(amountEt.text.toString().toLong().plus( userSnapshot.child("total")?.value as Long))
+                            userSnapshot.ref?.child("total")?.setValue(amount.plus( userSnapshot.child("total")?.value as Long))
                     }
 
                 })
--- app/src/main/java/com/nexflare/expensemanager/Adapter/UserAdapter.kt
+++ app/src/main/java/com/nexflare/expensemanager/Adapter/UserAdapter.kt
@@ -2,6 +2,7 @@ package com.nexflare.expensemanager.Adapter
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,8 @@ import android.view.ViewGroup
 import android.widget.TextView
 import com.nexflare.expensemanager.Activity.ExpenseActivity
 import com.nexflare.expensemanager.R
+import com.nexflare.expensemanager.R.drawable.expense
+import com.nexflare.expensemanager.R.id.colorView
 import com.nexflare.expensemanager.User
 
 /**
@@ -33,12 +36,19 @@ class UserAdapter(val context:Context,var arrayList: ArrayList<User>?):RecyclerV
         private val nameTv=itemView?.findViewById<TextView>(R.id.nameTv)
         private val amountTv=itemView?.findViewById<TextView>(R.id.amountTv)
         private val dateTv=itemView?.findViewById<TextView>(R.id.dateTv)
+        private val colorView=itemView?.findViewById<View>(R.id.colorView)
 
         fun bindData(position:Int){
             val user= arrayList?.get(position)
             nameTv?.text=user?.name
             amountTv?.text="\u20B9 "+user?.total
             dateTv?.visibility = View.GONE
+            if((user?.total?:0)<0){
+                colorView?.setBackgroundColor(Color.parseColor("#E93324"))
+            }
+            else{
+                colorView?.setBackgroundColor(Color.parseColor("#65e761"))
+            }
             itemView.setOnClickListener({
                 val intent= Intent(context, ExpenseActivity::class.java)
                 intent.putExtra("phone",user?.phone)
